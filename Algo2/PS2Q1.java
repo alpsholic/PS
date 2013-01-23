70447e652e29d8c063cad17d9739c9bc6692cb3a
Rammohan N <n.rammohan@gmail.com>
2013-01-23 23:18:32 +0100

    clustering

--- Algo2/PS2Q1.java
+++ Algo2/PS2Q1.java
@@ -25,6 +25,8 @@ ADVICE: If you're not getting the correct answer, try debugging your algorithm u
  */
 public class PS2Q1 {
 	static int[] parents;
+	/* parents[i] if -ve it is parent to itself. and the value represents its count- size of the cluster.
+	*/
 	static class Edge implements Comparable<Edge>{
 		int i;
 		int j;
@@ -44,18 +46,39 @@ public class PS2Q1 {
 			//return 0;
 		}
 	}
-	public static int numClusters(int[] p){
-		//num of distinct parents is the numClusters
-		int c = 0;
-		int[]p1 = new int[p.length];
-		for(int i = 0; i < p.length; i++)
-			p1[i] = -1;
-		for(int i = 0; i < p.length; i++)
-			p1[p[i]] = 1;
-		for(int i = 0; i < p.length; i++)
-			if (p[i]==1) c++;
-		return c;		
+	public static int find(int i){
+		while (parents[i]>0){
+			i = parents[i];
+		}
+		return i;
 	}
+	
+	public static void union(int i, int j){
+		//find parents of i and j..if same..they r in same cluster, update the count
+		//else in diff clusters..choose the one with less count and change its parent to the other parent
+		
+		int pi = find(i);
+		int pj = find(j);
+		
+		if (pi == pj) parents[pi] += -1;
+		else {
+			if (parents[pi] < parents[pj]){
+				//-ve counts so actually pi is larger than pj
+				parents[pi] += parents[pj];
+				parents[pj] = pi;
+			}else{
+				parents[pj] += parents[pi];
+				parents[pi] = pj;
+			}
+		}		
+	}
+	public static int numClusters(){
+		//num of -ve entries in parents is the no of clusters
+		int c = 0;
+		for(int i = 0; i < parents.length; i++)
+			if (parents[i]<0) c++;
+		return c;		
+	}	
 
 	/**
 	 * @param args
@@ -63,7 +86,7 @@ public class PS2Q1 {
 	public static void main(String[] args) {
 		ArrayList<Edge> edges = new ArrayList<Edge>();
 		ArrayList<ArrayList<Integer>> clusters = new ArrayList<ArrayList<Integer>>();
-		int k = 3;
+		int k = 4;
 		int n = 0;
 		try {
 			FileInputStream f = new FileInputStream(".//Algo2//clustering1.txt");
@@ -73,7 +96,7 @@ public class PS2Q1 {
 			parents = new int[n];			
 
 			for(int i = 0; i < n;i++){
-				parents[i] = i;				
+				parents[i] = -1;				
 			}
 			String str; int i,j,v;
 			while((str=b.readLine())!=null){
@@ -83,18 +106,19 @@ public class PS2Q1 {
 				edges.add(new Edge(i-1,j-1,v));
 			}
 			Collections.sort(edges);
-			for(Edge e : edges){				
-				//merge smaller cluster into bigger one if parents are different- if same, do nothing
-				int c1 = parents[e.i];
-				int c2 = parents[e.j];
-				if ( c1 == c2 ){
-					continue;
-				}else{
-					merge(e.i,e.j);
+			for(Edge e : edges){
+				union(e.i,e.j);
+				if (numClusters()==k) {
+					System.out.println("k clusters found ");
+					break;				
 				}
-				if (numClusters(parents)==k) break;
-				
 			}
+			//print the max distance among clusters..is actuallz the min distance between any two clusters
+			int max = Integer.MAX_VALUE;
+			for (Edge e : edges){
+				if (find(e.i)!=find(e.j)) max=Math.min(max, e.cost);				
+			}
+			System.out.println("max distance "+max);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
