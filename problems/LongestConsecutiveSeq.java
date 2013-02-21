a8642f4be608698e225ca68231ded5a028e44b80
Rammohan N <n.rammohan@gmail.com>
2013-02-22 00:06:28 +0100

    LSeq

--- problems/LongestConsecutiveSeq.java
+++ problems/LongestConsecutiveSeq.java
@@ -1,6 +1,7 @@
 package problems;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 /*
  * Longest Consecutive SequenceFeb 14
@@ -14,13 +15,7 @@ Your algorithm should run in O(n) complexity.
  */
 public class LongestConsecutiveSeq {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		//int[] input = {100, 4, 200, 1, 3, 2};
+	public static void old(){
 		int[] input = {1,2,0,1};
 		HashMap<Integer, Integer> lengths = new HashMap<Integer, Integer>();
 		int max = 1;
@@ -59,6 +54,49 @@ public class LongestConsecutiveSeq {
 		}
 
 		System.out.println("length " + max);
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] input = {100, 4, 200, 1, 3, 2};
+		//int[] input = {1,2,0,1};
+		//key == start, value = end of each seq seen so far (to find end of a seq starting at i)
+		HashMap<Integer, Integer> starting = new HashMap<Integer, Integer>();
+		//key=end, value = start (to find start of a seq ending at i)
+		HashMap<Integer, Integer> ending = new HashMap<Integer, Integer>();
+
+		for(int i : input){
+			//check if there is a seq starting with i+1 and another one ending at i-1
+			if (starting.containsKey(i+1) && ending.containsKey(i-1)){
+				//i merges both seqs
+				int s = ending.get(i-1);//start point of seq ending at i-1
+				int e = starting.get(i+1);//end point of seq starting at i+1
+				starting.put(s, e);
+				ending.put(e, s);				
+			}else if (starting.containsKey(i+1)){
+				int e = starting.get(i+1);
+				starting.put(i, e);
+				ending.put(e, i);
+			}else if (ending.containsKey(i-1)){
+				int s = ending.get(i-1);
+				ending.put(i, s);
+				starting.put(s, i);
+			}else {
+				starting.put(i, i);
+				ending.put(i, i);
+			}
+		}
+		int max = 1;
+		for (Entry<Integer, Integer> e : starting.entrySet()){
+			int i = e.getValue()-e.getKey()+1;
+			if (max < i){
+				max = i;				
+			}			
+		}
+		System.out.println("max : " + max);
 	}
 
 }
